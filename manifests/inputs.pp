6d257d4297d717cb6e35985f651879100496db1a
maartendq <mde_quick@hotmail.com>
2013-07-15 15:09:00 +0200

    First version of inputs.pp to add/remove monitor dirs

---
+++ manifests/inputs.pp
@@ -0,0 +1,58 @@
+# == Define: up_logrotate::rule
+#
+# Defines a logrotation schedule for a log file.
+#
+# === Parameters
+#
+#
+# === Examples
+#
+#
+define splunkforwarder::inputs (
+  $splunk_home       = $::splunkforwarder::splunk_home,
+  $service           = $::splunkforwarder::service,
+  $inputs_ensure     = undef,
+  $inputs_title      = undef,
+  $inputs_monitor    = undef,
+  $inputs_blacklist  = undef,
+  $inputs_sourcetype = undef,
+  $inputs_index      = undef,
+) {
+
+  require splunkforwarder
+
+  Augeas {
+    incl    => "${splunk_home}/etc/apps/search/local/inputs.conf",
+    lens    => 'Splunk.lns',
+    notify  => Service[$service],
+  }
+
+
+  case $inputs_ensure {
+    'present', default: {
+      augeas {"splunkforwarder::inputs::add-${inputs_title}":
+        changes => [
+          "set target[last()+1] ${inputs_monitor}",
+          "set target[last()]/blacklist ${inputs_blacklist}",
+          "set target[last()]/sourcetype ${inputs_sourcetype}",
+          "set target[last()]/index ${inputs_index}",
+        ],
+        onlyif  => "match target[. = '${inputs_monitor}'] size == 0",
+      }
+      augeas {"splunkforwarder::inputs::update-${inputs_title}":
+        changes => [
+          "set target[. = '${inputs_monitor}']/blacklist ${inputs_blacklist}",
+          "set target[. = '${inputs_monitor}']/sourcetype ${inputs_sourcetype}",
+          "set target[. = '${inputs_monitor}']/index ${inputs_index}",
+        ],
+        onlyif  => "match target[. = '${inputs_monitor}'] size > 0",
+      }
+    }
+    'absent': {
+      augeas {"splunkforwarder::inputs::rm-${inputs_title}":
+        changes => "rm target[. = '${inputs_monitor}']",
+      }
+    }
+
+  }
+}
