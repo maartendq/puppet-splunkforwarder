e9cfd40d502f56a95bd43d277bad8a6ef7fb3277
maartendq <mde_quick@hotmail.com>
2013-07-15 12:15:09 +0200

    Adding extra paramaters, defaults need to be better though

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,6 +4,7 @@
 #
 # == Parameters:
 #
+# [splunk_servers] : Comma seperated list of all receiving splunk servers
 #
 # == Actions:
 #  - Install splunkforwarder
@@ -13,9 +14,18 @@
 # == Sample Usage:
 #
 class splunkforwarder (
-  $package     = $::splunkforwarder::params::package,
-  $splunk_home = $::splunkforwarder::params::splunk_home,
-  $service     = $::splunkforwarder::params::service,
+  $owner                = $::splunkforwarder::params::owner,
+  $group                = $::splunkforwarder::params::group,
+  $package              = $::splunkforwarder::params::package,
+  $splunk_home          = $::splunkforwarder::params::splunk_home,
+  $service              = $::splunkforwarder::params::service,
+  $splunk_servergroup   = 'indexers',
+  $splunk_serverlist    = 'splunk.splunk.com:9997',
+  $splunk_inputs        = [
+    '/var/log/bla/',
+    '/var/log/bla2',
+  ],
+
 ) inherits splunkforwarder::params {
 
   include splunkforwarder::packages
